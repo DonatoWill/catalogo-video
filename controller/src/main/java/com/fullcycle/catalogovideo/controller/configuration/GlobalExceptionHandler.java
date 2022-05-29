c1d5de0e9bed08005667bd2e2cd48c1a1458a275
DonatoWill <donato_will@hotmail.com>
2022-05-28 23:35:32 -0300

    Adicionando global error handler e teste

--- controller/src/main/java/com/fullcycle/catalogovideo/controller/configuration/GlobalExceptionHandler.java
+++ controller/src/main/java/com/fullcycle/catalogovideo/controller/configuration/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.fullcycle.catalogovideo.controller.configuration;
 
 import com.fullcycle.catalogovideo.domain.exceptions.DomainException;
+import com.fullcycle.catalogovideo.domain.validation.Error;
 import com.fullcycle.catalogovideo.usecase.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -17,8 +22,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(value = {DomainException.class})
-    public ResponseEntity<?> handleDomainException(DomainException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<?> handleDomainException(final DomainException ex) {
+        return ResponseEntity.unprocessableEntity()
+                .body(ApiError.from(ex));
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        static ApiError from(final DomainException ex){
+            return new ApiError(ex.getMessage(), ex.getErrors());
+        }
     }
 
 }
--- controller/src/test/java/com/fullcycle/catalogovideo/controller/category/CategoryControllerTestIT.java
+++ controller/src/test/java/com/fullcycle/catalogovideo/controller/category/CategoryControllerTestIT.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.catalogovideo.controller.ControllerTest;
 import com.fullcycle.catalogovideo.domain.entity.CategoryID;
+import com.fullcycle.catalogovideo.domain.exceptions.DomainException;
+import com.fullcycle.catalogovideo.domain.validation.Error;
+import com.fullcycle.catalogovideo.domain.validation.handler.Notification;
 import com.fullcycle.catalogovideo.usecase.category.create.AbstractCreateCategoryUseCase;
 import com.fullcycle.catalogovideo.usecase.category.create.CreateCategoryInputData;
 import com.fullcycle.catalogovideo.usecase.category.create.CreateCategoryOutput;
@@ -24,6 +27,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Objects;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -82,4 +86,67 @@ public class CategoryControllerTestIT {
                 ));
     }
 
+    @Test
+    void givenAInvalidCommand_whenCallsCreateCategory_shouldReturnException() throws Exception {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCommand = new CreateCategoryInputData(expectedName, expectedDescription, expectedIsActive);
+
+        when(createUseCase.execute(any()))
+                .thenReturn(Left(Notification.create(new Error("Name should not be null"))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(aCommand));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", hasSize(1)),
+                        jsonPath("$.errors[0].message", equalTo("Name should not be null"))
+                );
+
+        verify(createUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.getName())
+                        && Objects.equals(expectedDescription, cmd.getDescription())
+                        && Objects.equals(expectedIsActive, cmd.getIsActive())
+        ));
+    }
+
+    @Test
+    void givenAInvalidCommand_whenCallsCreateCategory_shouldReturnDomainException() throws Exception {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCommand = new CreateCategoryInputData(expectedName, expectedDescription, expectedIsActive);
+
+        when(createUseCase.execute(any()))
+                .thenThrow(DomainException.with(new Error("Name should not be null")));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(aCommand));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", hasSize(1)),
+                        jsonPath("$.errors[0].message", equalTo("Name should not be null"))
+                );
+
+        verify(createUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.getName())
+                        && Objects.equals(expectedDescription, cmd.getDescription())
+                        && Objects.equals(expectedIsActive, cmd.getIsActive())
+        ));
+    }
 }
